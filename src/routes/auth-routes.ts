020f38216a56e369b46d0744963dba7255f52153
Rezky Desyafa <rezkydesyafa@gmail.com>
2023-09-02 21:42:00 +0700

    Cleaning code

--- src/routes/auth-routes.ts
+++ src/routes/auth-routes.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import { Token, login, logout, register } from '../controllers/auth-controller';
 const router = express.Router();
 
-router.post('/register', register);
+router.post('/registers', register);
 router.post('/login', login);
 router.delete('/logout', logout);
 router.get('/users/token', Token);
