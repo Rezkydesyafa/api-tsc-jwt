bf02b5d65a1788680d0ce50416a6a8e318594f14
Rezky Desyafa <rezkydesyafa@gmail.com>
2024-10-28 10:35:11 +0700

    fix running

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express, { Application } from 'express';
+import expres, { Application } from 'express';
 import dotenv from 'dotenv';
 import userRoute from './routes/user-route';
 import notesRoute from './routes/notes-route';
@@ -6,19 +6,19 @@ import authRoute from './routes/auth-routes';
 import cookieParser from 'cookie-parser';
 
 dotenv.config();
-const app: Application = express();
+const app: Application = expres();
 const port = process.env.PORT;
 
 app.use(cookieParser());
-app.use(express.json());
+app.use(expres.json());
 app.use('/api', authRoute);
 app.use('/api', userRoute);
 app.use('/api', notesRoute);
 app.get('/', (req, res) => {
   res.status(200).json({
     status: 200,
-    name: 'Ekiww-Public-Api',
-    version: '1.0.0',
+    name: 'Notes-Resfull-Api',
+    version: '1.0.1',
     docs_link: 'https://comingsoon.com/docs',
     health_check: '100',
     is_open: true,
